5240d0f0e2c815197cb64d6556bd3b8241ce7889
menescalnickolas <menescalnickolas@gmail.com>
2025-02-22 21:02:39 +0100

    Worked on Task 2.5

--- recipes/models.py
+++ recipes/models.py
@@ -11,9 +11,12 @@ difficulty_choice = (
 class Recipes(models.Model):
   recipe_id = models.PositiveIntegerField()
   name = models.CharField(max_length=50)
+  description = models.TextField()
   cooking_time = models.PositiveIntegerField(help_text='in minutes')
   ingredients = models.TextField()
+  preparation_method = models.TextField()
   difficulty = models.CharField(max_length=12, choices=difficulty_choice)
-  
+  pic = models.ImageField(upload_to='recipes', default='no_picture.jpg')
+
   def __str__(self):
     return str(self.name)
