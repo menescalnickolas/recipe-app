1ca4743187e4cd687b2cdf11f2ce63b3df17baa5
menescalnickolas <menescalnickolas@gmail.com>
2025-02-23 22:49:32 +0100

    Worked on Task 2.6

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,7 +6,7 @@ from .views import RecipeDetailView
 app_name = 'recipes'
 
 urlpatterns = [
-   path('', home),
+   path('', home, name = 'home'),
    path('list/', RecipesListView.as_view(), name='list'),
    path('list/<pk>', RecipeDetailView.as_view(), name='detail')
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Recipes
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 def home (request):
   return render(request, 'recipes/recipes_home.html')
 
-class RecipesListView(ListView):
+class RecipesListView(LoginRequiredMixin, ListView):
   model = Recipes
   template_name = 'recipes/main.html'
 
-class RecipeDetailView(DetailView):
+class RecipeDetailView(LoginRequiredMixin, DetailView):
   model = Recipes
   template_name = 'recipes/detail.html'
