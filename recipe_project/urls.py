4c0de531bafcb5c19bfb1aa17e68a8ad42b7be77
menescalnickolas <menescalnickolas@gmail.com>
2025-03-04 16:43:10 +0100

    Debugging images issue

--- recipe_project/urls.py
+++ recipe_project/urls.py
@@ -31,5 +31,5 @@ urlpatterns = [
     path ('about/', about_me, name='about')
 ]
 
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
