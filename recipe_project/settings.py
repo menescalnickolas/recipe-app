b3f73ef58492fc4f34f1a5370ac095958198ded5
menescalnickolas <menescalnickolas@gmail.com>
2025-03-04 20:51:49 +0100

    debugging

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -160,3 +160,5 @@ CLOUDINARY_STORAGE = {
     'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
     'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
 }
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
